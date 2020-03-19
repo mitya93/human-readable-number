ec3fa2129f4abfe3c5a4bade92e36e08cfe5aac2
mitya93 <dimakovalvdv@mail.ru>
2020-03-19 22:18:18 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function toReadable (number) {
-  
-}
+  let words = [
+    [' ', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+    ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+    [' ', ' ', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  ]
+
+      return hundreds_numbers(number).trim();
+      function ones_numbers(number){
+        if (!number % 10) {
+          return;
+        } else {
+          return ' ' + words[0][number % 10]
+        }
+      }  
+      function tens_numbers(number) {
+        if (number < 10) {
+          return words[0][number];
+        } else if  (number >= 20 && number <= 99){
+         return words[2][Math.floor(number / 10)] + ones_numbers(number);
+        } else {
+          return words[1][number - 10];
+        }
+       }
+      function hundreds_numbers (number) {
+        if (number === 0) {
+          return 'zero';
+        }
+         else if (number >= 100) {
+          return words[0][Math.floor(number/100)] + ' hundred' + ' ' + tens_numbers(Math.floor(number % 100));
+        }
+        else {
+          return tens_numbers(number);
+        }
+      }     
+     } 
+    
+     
+
